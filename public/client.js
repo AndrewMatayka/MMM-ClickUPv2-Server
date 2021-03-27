2e58998847d07e65ae0adab84865c4744294d6d3
Andrew Matayka <andrewmatayka2001@gmail.com>
2021-03-26 23:58:50 -0500

    Initial Commit: Created Server with Working AUTH and Access Code Retrieval

---
+++ public/client.js
@@ -0,0 +1,73 @@
+/* WEB CLIENT FOR OAUTHv2 AUTHENTICATION */
+console.log('Client-side code running');
+
+//Set all HTML elements we need to variables
+const button = document.getElementById('myButton');
+const textarea = document.getElementById('myTextarea');
+const codeVal = document.getElementById('codeVal');
+const test = document.getElementById('test');
+
+let token = "";
+
+//Create a Centered Popup on the Screen
+function centeredPopup(url, winName, w, h, scroll) {
+    let LeftPosition = (screen.width) ? (screen.width - w) / 2 : 0;
+    let TopPosition = (screen.height) ? (screen.height - h) / 2 : 0;
+    let settings =
+        'height=' + h + ',width=' + w + ',top=' + TopPosition + ',left=' + LeftPosition + ',scrollbars=' + scroll + ',resizable'
+    return window.open(url, winName, settings);
+}
+
+//Show Authentication Window
+function showAuthWindow(options) {
+    options.windowName = options.windowName || "Authentication";
+    options.windowOptions = options.windowOptions || 'location=0, status=0, width=500, height=700';
+    options.callback = options.callback || function () {
+        window.location.reload();
+    };
+
+    let leftDomain = false;
+    //let oauthWindow = window.open(options.path, options.windowName, options.windowOptions);
+    let oauthWindow = centeredPopup(options.path, options.windowName, options.windowOptions.width, options.windowOptions.height, options.windowOptions.scroll);
+    let oauthInterval = window.setInterval(function () {
+        try {
+            if (oauthWindow.document.domain === document.domain) {
+                if (leftDomain && oauthWindow.document.readyState === "complete") {
+                    clearInterval(oauthInterval);
+                    options.callback(oauthWindow.document.URL);
+                    oauthWindow.close();
+                }
+            }
+        } catch (e) {
+            if (oauthWindow.closed) {
+                clearInterval(oauthInterval);
+            }
+            leftDomain = true;
+        }
+    }, 500);
+}
+
+//Create Event listener for Button
+let client_id = "";
+if (button !== null) {
+    button.addEventListener('click', function (e) {
+        client_id = textarea.value;
+        if (client_id.length > 0) {
+            createAuth();
+        } else {
+            alert("You need to enter a Client ID!");
+        }
+    });
+}
+
+//Make call request to get Access Code
+function createAuth() {
+    showAuthWindow({
+        path: "https://app.clickup.com/api?client_id=" + client_id + "&redirect_uri=127.0.0.1/loading.html",
+        callback: function (data) {
+            token = data.substring(data.indexOf('=') + 1);
+            codeVal.innerHTML = token;
+            test.style.display = "block";
+        }
+    });
+}
---
+++ server.js
@@ -0,0 +1,38 @@
+/* WEB SERVER FOR OAUTHv2 AUTHENTICATION */
+console.log('Server-side code running');
+
+/* REQUIRE FOR WEB SERVER & FILE MANAGEMENT */
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+const https = require('https');
+const express = require('express');
+
+//Using Express
+const app = express();
+
+//Including Loading HTML File in Root
+app.get('/loading.html', function (req, res) {
+    res.sendFile(path.join(__dirname + '/loading.html'));
+});
+
+//Redirect HTTP to HTTPS
+app.enable("trust proxy");
+app.use(function(request, response, next) {
+    if (!request.secure) {
+        return response.redirect("https://" + request.headers.host + request.url);
+    }
+    next();
+});
+
+//Use the Public Directory
+app.use(express.static('public'));
+
+// SSL Certs
+const httpsOptions = { key: fs.readFileSync('public/sslcert/key.pem'), cert: fs.readFileSync('public/sslcert/cert.pem') };
+
+//Create Servers
+app.listen(80);
+https.createServer(httpsOptions, app).listen(443, function () {
+    console.log('Express HTTPS server listening on port ' + 443);
+});
